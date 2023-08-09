5d4ee4aa29f82ae269dd3b1beedee11293a76081
D <dhifa.aghisni@gmail.com>
2023-08-10 02:41:20 +0700

    add linux_minimal_setup_nvim.sh

---
+++ linux_minimal_setup_nvim.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# apt install neovim git node go
+# Go to home directory
+cd ~
+
+# Install neovim
+sudo apt install git
+
+# Create .config/nvim folder if not exists
+mkdir -p ~/.config/nvim
+
+# Download init.vim and coc-settings.json from GitHub repository
+curl -LJO https://github.com/difaagh/init.vim/raw/main/init.vim
+curl -LJO https://github.com/difaagh/init.vim/raw/main/coc-settings.json
+
+mv init.vim ~/.config/nvim/
+mv coc-settings.json ~/.config/nvim/
+
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Open Neovim
+nvim +PlugInstall +qall
+
